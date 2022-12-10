1201e2c2b8b3f59f490e37f732ade87b8d054e84
Reka <torokreka12@yahoo.com>
2022-12-10 11:40:17 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String CNP = "2810812142609";
+
+        if (CNP.length() != 13) {
+            System.out.println("CNP invalid");
+        }
+
+
+    }
+}
